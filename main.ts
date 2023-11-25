b3131453b64029f2d3863a28cb13ab715302c307
Gabriel Pampolha <pampolhag@gmail.com>
2023-11-25 20:42:29 -0300

    feat: deck download now happens in parallel to deck search

--- main.ts
+++ main.ts
@@ -1,43 +1,22 @@
 import Puppeteer, { Browser, Page } from "puppeteer";
 import { Cluster } from "puppeteer-cluster";
 import Path from "path";
-import { argv } from "yargs";
+import Yargs from "yargs/yargs";
+import { hideBin } from "yargs/helpers";
+import Os from "os";
 
-const decksUrl =
+const deckSourceUrl =
   "https://ygoprodeck.com/deck-search/?&_sft_category=master%20duel%20decks&banlist=&offset=0";
 
-const collectUrls = async (page: Page) => {
-  const deckUrlArray = await page.evaluate(() => {
+const collectPageLinks = async (page: Page) => {
+  return await page.evaluate(() => {
     const anchorList = document.querySelectorAll(
       "div.deck_article-card-container > a"
     );
     // @ts-ignore
     return Array.from(anchorList).map((el) => el.href);
   });
-
-  return deckUrlArray;
 };
-
-async function getDecks(browser: Browser, limit: number) {
-  const deckUrls = [];
-  const page = await browser.newPage();
-  await page.goto(decksUrl, { waitUntil: "networkidle0" });
-  while (deckUrls.length < limit) {
-    deckUrls.push(...(await collectUrls(page)));
-    await page.click("#pagination-elem > ul > li.page-item.prevDeck > a");
-    try {
-      await page.waitForSelector("div.deck_article-card-container > a", {
-        visible: true,
-      });
-    } catch {
-      console.log("Did not find any decks in the page. Ending search loop.");
-      break;
-    }
-  }
-  await page.close();
-  if (deckUrls.length > limit) return deckUrls.slice(0, limit);
-  return deckUrls;
-}
 
 const configureDownloadBehaviour = async (page: Page, folderName: string) => {
   const cdp = await page.createCDPSession();
@@ -55,46 +34,63 @@ const findFailedDownloads = (decks: string[], downloaded: string[]) => {
   return failed;
 };
 
-async function downloadDecks(
+const downloadDeck = async (
   browser: Browser,
-  decks: string[],
-  maxConcurrency = 5,
+  url: string,
+  downloadedUrlArray: string[],
   folderName = "decks"
-) {
-  const cluster = await Cluster.launch({
-    concurrency: Cluster.CONCURRENCY_CONTEXT,
-    maxConcurrency,
-  });
-  const downloaded: string[] = [];
-  for (const deckUrl of decks) {
-    const page = await browser.newPage();
-    cluster.queue(async () => {
-      await configureDownloadBehaviour(page, folderName);
-      await page.goto(deckUrl, { waitUntil: "networkidle0" });
-      await page.click("#dropdownMenuButton");
-      await page.click('a[onclick~="downloadYDK()"]');
-      await page.waitForNetworkIdle({ timeout: 5000 });
-      downloaded.push(deckUrl);
-      await page.close;
-    });
+) => {
+  const page = await browser.newPage();
+  await configureDownloadBehaviour(page, folderName);
+  await page.goto(url, { waitUntil: "networkidle0" });
+  await page.click("#dropdownMenuButton");
+  await page.click('a[onclick~="downloadYDK()"]');
+  await page.waitForNetworkIdle({ timeout: 5000 });
+  await page.close();
+  downloadedUrlArray.push(url);
+};
+
+async function getDecks(browser: Browser, cluster: Cluster, limit: number) {
+  const deckUrlArray: string[] = [];
+  const downloadedUrlArray: string[] = [];
+  const page = await browser.newPage();
+  await page.goto(deckSourceUrl, { waitUntil: "networkidle0" });
+  while (deckUrlArray.length < limit) {
+    const pageDeckUrlArray = await collectPageLinks(page);
+    deckUrlArray.push(...pageDeckUrlArray);
+    for (const url of pageDeckUrlArray) {
+      cluster.queue(async () => downloadDeck(browser, url, downloadedUrlArray));
+    }
+    await page.click("#pagination-elem > ul > li.page-item.prevDeck > a");
+    try {
+      await page.waitForSelector("div.deck_article-card-container > a", {
+        visible: true,
+      });
+    } catch {
+      console.log("Did not find any decks in the page. Ending search loop.");
+      break;
+    }
   }
-  await cluster.idle();
-  await cluster.close();
-  return downloaded;
+  await page.close();
+  if (deckUrlArray.length > limit) return deckUrlArray.slice(0, limit);
+  return {
+    deckUrls: deckUrlArray,
+    downloaded: downloadedUrlArray,
+  };
 }
 
 Puppeteer.launch().then(async (browser) => {
-  // @ts-ignore
-  const deckLimit = (await argv.limit) || 500;
-  const decks = await getDecks(browser, deckLimit);
-  console.log(`Got ${decks.length} decks, attempting to download them...`);
-  const downloaded = await downloadDecks(browser, decks);
-  let failed = findFailedDownloads(decks, downloaded);
-  while (failed.length) {
-    console.log(`Some decks have failed to download: ${failed}.\nRetrying...`);
-    const retryDownloaded = await downloadDecks(browser, failed);
-    failed = findFailedDownloads(failed, retryDownloaded);
-  }
+  const argv = await Yargs(hideBin(process.argv)).option({
+    limit: { type: "number" },
+  }).argv;
+  const deckLimit = argv.limit || 500;
+  const cluster = await Cluster.launch({
+    concurrency: Cluster.CONCURRENCY_CONTEXT,
+    maxConcurrency: Os.cpus().length,
+  });
+  const decks = await getDecks(browser, cluster, deckLimit);
+  await cluster.idle();
+  await cluster.close();
   console.log("Downloaded all decks");
   await browser.close();
 });
