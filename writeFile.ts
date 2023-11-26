56fe0e797a6e8421ebc43506945091ab24996437
Gabriel Pampolha <pampolhag@gmail.com>
2023-11-25 23:28:41 -0300

    feat: scraper now copies ydke, parse it and save it to a file + implemented retry

---
+++ writeFile.ts
@@ -0,0 +1,39 @@
+import { writeFile, mkdir } from "fs/promises";
+import Path from "path";
+
+interface Deck {
+  main: Uint32Array;
+  extra: Uint32Array;
+  side: Uint32Array;
+}
+
+async function writeDeckToFile(
+  deck: Deck,
+  folderName: string,
+  fileName: string
+) {
+  let ydkContent = "";
+
+  ydkContent += "#main\n";
+  ydkContent += deck.main.join("\n") + "\n";
+
+  ydkContent += "#extra\n";
+  ydkContent += deck.extra.join("\n") + "\n";
+
+  ydkContent += "!side\n";
+  ydkContent += deck.side.join("\n") + "\n";
+
+  await mkdir(folderName, { recursive: true });
+  await writeFile(
+    Path.resolve(
+      folderName,
+      fileName.trim().replace(/[<>:"\/\\|?*\x00-\x1F]/g, "s")
+    ),
+    ydkContent,
+    {
+      flag: "wx",
+    }
+  );
+}
+
+export default writeDeckToFile;
