66f2ab75eee14f158e6cacdfba600a64bce926a7
Gabriel Pampolha <pampolhag@gmail.com>
2023-11-26 14:52:19 -0300

    doc: informed defautl values to arguments

--- src/arguments.ts
+++ src/arguments.ts
@@ -5,7 +5,9 @@ const loadArguments = async () =>
   await Yargs(hideBin(process.argv)).option({
     limit: {
       type: "number",
-      describe: "The nonnegative number of decks to be fetched and saved.",
+      describe: `The nonnegative number of decks to be fetched and saved.
+      
+      This defaults to 500 decks.`,
     },
     range: {
       type: "number",
@@ -15,17 +17,23 @@ const loadArguments = async () =>
       0 - All decks
       1 - Featured builders
       2 - High quality deck primer
-      3 - Premium supporter decks`,
+      3 - Premium supporter decks
+      
+      This defaults to all decks.`,
     },
     initialDate: {
       type: "string",
       describe: `The starting date of the time range in which to search the deck for.
-      Must be in yyyy-MM-dd format.`,
+      Must be in yyyy-MM-dd format.
+      
+      This defaults to no initial range.`,
     },
     finalDate: {
       type: "string",
       describe: `The final date of the time range in which to search the deck for.
-      Must be in yyyy-MM-dd format.`,
+      Must be in yyyy-MM-dd format.
+      
+      This defaults to no final range.`,
     },
   }).argv;
 
