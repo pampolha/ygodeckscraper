21407ba61064d65787805247e3d9fed08c7ba507
Gabriel Pampolha <pampolhag@gmail.com>
2023-11-25 23:28:41 -0300

    feat: scraper now copies ydke, parse it and save it to a file + implemented retry

--- main.ts
+++ main.ts
@@ -1,9 +1,10 @@
 import Puppeteer, { Browser, Page } from "puppeteer";
 import { Cluster } from "puppeteer-cluster";
-import Path from "path";
 import Yargs from "yargs/yargs";
 import { hideBin } from "yargs/helpers";
 import Os from "os";
+import writeDeckToFile from "./writeFile";
+import * as Ydke from "ydke";
 
 const deckSourceUrl =
   "https://ygoprodeck.com/deck-search/?&_sft_category=master%20duel%20decks&banlist=&offset=0";
@@ -18,14 +19,6 @@ const collectPageLinks = async (page: Page) => {
   });
 };
 
-const configureDownloadBehaviour = async (page: Page, folderName: string) => {
-  const cdp = await page.createCDPSession();
-  await cdp.send("Page.setDownloadBehavior", {
-    behavior: "allow",
-    downloadPath: Path.resolve(folderName),
-  });
-};
-
 const findFailedDownloads = (decks: string[], downloaded: string[]) => {
   const failed = [];
   for (const deck of decks) {
@@ -34,7 +27,7 @@ const findFailedDownloads = (decks: string[], downloaded: string[]) => {
   return failed;
 };
 
-const downloadDeck = async (
+const saveDeck = async (
   browser: Browser,
   url: string,
   downloadedUrlArray: string[],
@@ -42,11 +35,25 @@ const downloadDeck = async (
 ) => {
   try {
     const page = await browser.newPage();
-    await configureDownloadBehaviour(page, folderName);
     await page.goto(url, { waitUntil: "networkidle0" });
-    await page.click("#dropdownMenuButton");
-    await page.click('a[onclick~="downloadYDK()"]');
-    await page.waitForNetworkIdle({ timeout: 5000 });
+    const deckName = await page.evaluate(
+      () =>
+        document.querySelector("div.deck-metadata-container.deck-bgimg > h1")
+          ?.textContent
+    );
+    const deckAuthor = await page.evaluate(
+      () =>
+        document.querySelector(
+          "div.deck-metadata-container.deck-bgimg > div:nth-child(2) > span > a:nth-child(2)"
+        )?.textContent
+    );
+    // @ts-ignore
+    const ydke = (await page.evaluate(() => createYdkeUri())) as string;
+    await writeDeckToFile(
+      Ydke.parseURL(ydke),
+      folderName,
+      `${deckName} - ${deckAuthor}`
+    );
     await page.close();
     downloadedUrlArray.push(url);
   } catch (err) {
@@ -61,11 +68,17 @@ async function getDecks(browser: Browser, cluster: Cluster, limit: number) {
   await page.goto(deckSourceUrl, { waitUntil: "networkidle0" });
   while (deckUrlArray.length < limit) {
     try {
-      const pageDeckUrlArray = await collectPageLinks(page);
+      let pageDeckUrlArray = await collectPageLinks(page);
+      if (pageDeckUrlArray.length + deckUrlArray.length > limit) {
+        pageDeckUrlArray = pageDeckUrlArray.slice(
+          0,
+          deckUrlArray.length + pageDeckUrlArray.length - limit
+        );
+      }
       deckUrlArray.push(...pageDeckUrlArray);
       for (const url of pageDeckUrlArray) {
-        cluster.queue(async () =>
-          downloadDeck(browser, url, downloadedUrlArray)
+        await cluster.queue(async () =>
+          saveDeck(browser, url, downloadedUrlArray)
         );
       }
       await page.click("#pagination-elem > ul > li.page-item.prevDeck > a");
@@ -81,7 +94,6 @@ async function getDecks(browser: Browser, cluster: Cluster, limit: number) {
     }
   }
   await page.close();
-  if (deckUrlArray.length > limit) return deckUrlArray.slice(0, limit);
   return {
     deckUrls: deckUrlArray,
     downloaded: downloadedUrlArray,
@@ -98,6 +110,18 @@ Puppeteer.launch().then(async (browser) => {
     maxConcurrency: Os.cpus().length,
   });
   const decks = await getDecks(browser, cluster, deckLimit);
+  await cluster.idle();
+  let failedArray = findFailedDownloads(decks.deckUrls, decks.downloaded);
+
+  while (failedArray.length) {
+    const retryArray: string[] = [];
+    console.log(`${failedArray.length} decks failed to download, retrying...`);
+    for (const deck of failedArray) {
+      await cluster.queue(async () => saveDeck(browser, deck, retryArray));
+    }
+    await cluster.idle();
+    failedArray = findFailedDownloads(failedArray, retryArray);
+  }
   await cluster.idle();
   await cluster.close();
   console.log("Downloaded all decks");
